dc5daae5abfe63c7f58d18aa8d495d5cf48719df
unknown <moh.ali.1593@gmail.com>
2021-12-23 20:10:48 +0200

    CARD IS PIXEL PERFECT ON INITIAL POSITION!

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -49,9 +49,9 @@ const Card = ({
 
   return (
     <div
-      className={`py-2  px-2 px-md-3 d-flex flex-row justify-content-start align-items-start hr-card-container `}
+      className={` px-2 px-md-3 d-flex flex-row justify-content-start align-items-start hr-card-container `}
     >
-      <div className="card-img-options d-flex flex-column justify-content-center align-items-center gap-4 gap-md-2">
+      <div className="card-img-options d-flex flex-column justify-content-center align-items-center gap-3 gap-md-2">
         <img className="hr-card-img" src={img} alt="client" />
         <div className="image-options-container">
           <MdModeEditOutline className="img-options" />
@@ -59,7 +59,7 @@ const Card = ({
           <MdDeleteForever onClick={handleDelete} className="img-options" />
         </div>
       </div>
-      <div className="card-info ">
+      <div className="card-info ms-2 ">
         <h5 className="text-dark text-truncate">
           {name && name[0].toUpperCase() + name.substring(1)}
         </h5>
@@ -68,7 +68,7 @@ const Card = ({
         <div className={`attendance-statue ${attendance}`}>
           {attendance && attendance[0].toUpperCase() + attendance.substring(1)}
         </div>
-        <div className="position-absolute bottom-0 end-0 p-2 d-flex flex-row justify-content-center align-items-center gap-1">
+        <div className="position-absolute bottom-0 end-0 pb-2 pe-3 d-flex flex-row justify-content-center align-items-center gap-1">
           <div className="card-icon-container d-flex justify-content-center align-items-center">
             <MdEmail className="card-icon" />
           </div>
@@ -86,28 +86,6 @@ const Card = ({
           </OverlayTrigger>
         </div>
       </div>
-      {/* {AdditionalInfo && (
-        <div className="d-flex flex-row justify-content-center align-items-center p-2 additional-info">
-          <Row>
-            <Col className="d-flex flex-column align-items-start">
-              <span className="add-info-title">Office</span>
-              <span className="mb-2 add-info-description">{office}</span>
-              <span className="add-info-title">Joining Date</span>
-              <span className="mb-2 add-info-description">{sDate}</span>
-            </Col>
-            <Col className="d-flex flex-column align-items-start">
-              <span className="add-info-title">Role</span>
-              <span className="mb-2 add-info-description">{role}</span>
-              <span className="add-info-title">Manager</span>
-              <span className="mb-2 add-info-description">{dManager}</span>
-            </Col>
-            <Col className="d-flex flex-column align-items-start">
-              <span className="add-info-title">Copied Manager</span>
-              <span className="mb-2 add-info-description">{dManager}</span>
-            </Col>
-          </Row>
-        </div>
-      )} */}
     </div>
   );
 };
